1cb078f7f4a3675d4273d36e3f2d6ad8035fad5c
Ciczau <wiktor.michalski@outlook.com>
2024-06-08 17:46:22 +0200

    chore: cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,18 @@
-import 'dotenv/config'
-import Koa from 'koa'
-import interpreterRouter from './src/routes/interpreter/index.ts'
-import bodyParser from 'koa-bodyparser'
+import "dotenv/config";
+import Koa from "koa";
+import interpreterRouter from "./src/routes/interpreter/index.ts";
+import bodyParser from "koa-bodyparser";
+import cors from "@koa/cors";
 
 const app = new Koa();
 
+app.use(cors({ origin: "*" }));
 app.use(bodyParser());
 app.use(interpreterRouter.routes());
 app.use(interpreterRouter.allowedMethods());
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server started at port: ${PORT}`)
-})
+  console.log(`Server started at port: ${PORT}`);
+});
